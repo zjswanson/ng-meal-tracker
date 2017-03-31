df88d43965fe086516858c46135a33b3549b9506
Zach <zach.j.swanson@gmail.com>
2017-03-31 13:28:11 -0700

    refactor add template to clear values after submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,6 @@ export class AppComponent implements OnInit {
     this.mealService.getMeals().subscribe(result => {
       let mealArray: Meal[] = [];
       result.forEach(function(meal) {
-        console.log(meal.$key);
         let newMeal = new Meal(meal.name, meal.notes, meal.calories);
         newMeal.$key = meal.$key
         mealArray.push(newMeal);
